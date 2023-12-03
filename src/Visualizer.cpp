c094ea2c3510abd4807c456b862aef7b5651069d
eduardo98m <eduardo98m@gmail.com>
2023-12-03 22:39:12 +0900

    Added comments + Custom Camera working (WIP)

--- src/Visualizer.cpp
+++ src/Visualizer.cpp
@@ -1,84 +1,95 @@
 #include "Visualizer.hpp"
 
-Visualizer::Visualizer(int screen_width, int screen_height, const char* title) : 
-    screen_width_(screen_width), 
-    screen_height_(screen_height), 
-    title_(title),
-    wireframe_mode_(false), 
-    focused_object_index_(-1)
-    {
+Visualizer::Visualizer(int screen_width, int screen_height, const char *title) : screen_width_(screen_width),
+                                                                                 screen_height_(screen_height),
+                                                                                 title_(title),
+                                                                                 wireframe_mode_(false),
+                                                                                 focused_object_index_(-1)
+{
     InitWindow(screen_width_, screen_height_, title_);
-    
+
     SetTargetFPS(60);
     rlImGuiSetup(true); // Setup ImGui
 
     this->shader_target_ = LoadRenderTexture(screen_width_, screen_height_);
 }
 
-Visualizer::~Visualizer() {
+Visualizer::~Visualizer()
+{
     this->close();
 }
-void Visualizer::set_up_camera() {
-    this->camera_ = (Camera3D){ { 0.0f, 10.0f, 10.0f }, { 0.0f, 0.0f, 0.0f }, { 0.0f, 1.0f, 0.0f }, 45.0f, CAMERA_PERSPECTIVE };
+void Visualizer::set_up_camera()
+{
+    this->camera_ = (Camera3D){{0.0f, 10.0f, 10.0f}, {0.0f, 0.0f, 0.0f}, {0.0f, 1.0f, 0.0f}, 45.0f, CAMERA_PERSPECTIVE};
 }
 
-void Visualizer::set_camera_focus() {
-    if (this->focused_object_index_ != -1) {
+void Visualizer::set_camera_focus()
+{
+    if (this->focused_object_index_ != -1)
+    {
         this->camera_.target = this->visual_objects_[this->focused_object_index_].position;
-        // Also set the camera position to be a bit further away from the object
-        //this->camera_.position = Vector3Add(this->visual_objects_[this->focused_object_index_].position, {0.0f, 0.0f, 10.0f});
     }
 }
 
-uint Visualizer::add_visual_object(VisualObject vis_object) {
+uint Visualizer::add_visual_object(VisualObject vis_object)
+{
     this->visual_objects_.push_back(vis_object);
 
     return this->visual_objects_.size() - 1;
 }
 
-void Visualizer::update_visual_object_position_orientation(uint index, Vector3 position, Quaternion orientation) {
+void Visualizer::update_visual_object_position_orientation(uint index, Vector3 position, Quaternion orientation)
+{
     this->visual_objects_[index].position = position;
     this->visual_objects_[index].orientation = orientation;
 }
 
-void Visualizer::update_visual_object_position_orientation_scale(uint index, Vector3 position, Quaternion orientation, Vector3 scale) {
+void Visualizer::update_visual_object_position_orientation_scale(uint index, Vector3 position, Quaternion orientation, Vector3 scale)
+{
     this->visual_objects_[index].position = position;
     this->visual_objects_[index].orientation = orientation;
     this->visual_objects_[index].model.transform = MatrixScale(scale.x, scale.y, scale.z);
 }
 
-void Visualizer::update_visual_object_scale(uint index, Vector3 scale) {
+void Visualizer::update_visual_object_scale(uint index, Vector3 scale)
+{
     this->visual_objects_[index].model.transform = MatrixScale(scale.x, scale.y, scale.z);
 }
 
-void Visualizer::remove_visual_object(uint index) {
+void Visualizer::remove_visual_object(uint index)
+{
     this->visual_objects_.erase(this->visual_objects_.begin() + index);
 }
 
-void Visualizer::clear_visual_objects() {
+void Visualizer::clear_visual_objects()
+{
     this->visual_objects_.clear();
 }
 
-void Visualizer::draw_shader() {
-    if (this->shader_loaded_) {
+void Visualizer::draw_shader()
+{
+    if (this->shader_loaded_)
+    {
         BeginShaderMode(this->shader_);
         // NOTE: Render texture must be y-flipped due to default OpenGL coordinates (left-bottom)
         // TODO: Fix this with the camera target
 
-        DrawTextureRec(this->shader_target_.texture, 
-                    (Rectangle){ 0, 0, (float)this->shader_target_.texture.width, (float)-this->shader_target_.texture.height }, 
-                    (Vector2){ 0, 0 }, WHITE);
-        
-    EndShaderMode();
+        DrawTextureRec(this->shader_target_.texture,
+                       (Rectangle){0, 0, (float)this->shader_target_.texture.width, (float)-this->shader_target_.texture.height},
+                       (Vector2){0, 0}, WHITE);
+
+        EndShaderMode();
     }
-    else {
-        DrawTextureRec(this->shader_target_.texture, 
-                    (Rectangle){ 0, 0, (float)this->shader_target_.texture.width, (float)-this->shader_target_.texture.height }, 
-                    (Vector2){ 0, 0 }, WHITE);
+    else
+    {
+        DrawTextureRec(this->shader_target_.texture,
+                       (Rectangle){0, 0, (float)this->shader_target_.texture.width, (float)-this->shader_target_.texture.height},
+                       (Vector2){0, 0}, WHITE);
     }
 }
 
-void Visualizer::draw_gui() {
+void Visualizer::draw_gui()
+{
     rlImGuiBegin();
 
     // Draw the GUI
@@ -100,14 +111,18 @@ void Visualizer::draw_gui() {
     // ImGui::Text("Focused object");
     // ImGui::InputInt("Focused object index", &this->focused_object_index_);
     // Create a drop down menu for the focused object
-    if (ImGui::BeginCombo("Focused object", TextFormat("Object %d", this->focused_object_index_))) {
-        for (int n = 0; n < this->visual_objects_.size(); n++) {
+    if (ImGui::BeginCombo("Focused object", TextFormat("Object %d", this->focused_object_index_)))
+    {
+        for (int n = 0; n < this->visual_objects_.size(); n++)
+        {
             bool is_selected = (this->focused_object_index_ == n);
-            
-            if (ImGui::Selectable(TextFormat("Object %d", n), is_selected)) {
+
+            if (ImGui::Selectable(TextFormat("Object %d", n), is_selected))
+            {
                 this->focused_object_index_ = n;
             }
-            if (is_selected) {
+            if (is_selected)
+            {
                 ImGui::SetItemDefaultFocus();
             }
         }
@@ -115,160 +130,221 @@ void Visualizer::draw_gui() {
     }
     // End the GUI
     ImGui::End();
-
-
-     
+    rlImGuiEnd();
 }
 
-void Visualizer::update(){
+void Visualizer::update_camera()
+{
+    // Camera movement
+    Vector3 cameraFront = Vector3Subtract(this->camera_.target, this->camera_.position);
+    Vector3 cameraRight = Vector3Normalize(Vector3CrossProduct(cameraFront, this->camera_.up));
+
+    float cameraSpeed = 0.1f;
+    float cameraRotationSpeed = 0.2f;
+
+    Vector3 movement = {0.0f, 0.0f, 0.0f};
+    Vector3 rotation = {0.0f, 0.0f, 0.0f};
+
+    if (IsKeyDown(KEY_W))
+        movement.x += 1.0f;
+    if (IsKeyDown(KEY_S))
+        movement.x -= 1.0f;
+    if (IsKeyDown(KEY_A))
+        movement.y -= 1.0f;
+    if (IsKeyDown(KEY_D))
+        movement.y += 1.0f;
+
+    // Vertical movement with Shift (up) and Control (down) keys
+    if (IsKeyDown(KEY_LEFT_SHIFT))
+        movement.z += 1.0f;
+    if (IsKeyDown(KEY_LEFT_CONTROL))
+        movement.z -= 1.0f;
+    if (IsKeyDown(KEY_Q))
+        movement.z += 1.0f;
+    if (IsKeyDown(KEY_E))
+        movement.z -= 1.0f;
+
+    // Camera rotation with the mouse
+    if (IsMouseButtonPressed(MOUSE_RIGHT_BUTTON))
+        DisableCursor();
+    if (IsMouseButtonReleased(MOUSE_RIGHT_BUTTON))
+        EnableCursor();
+
+    if (IsMouseButtonDown(MOUSE_RIGHT_BUTTON))
+    {
+        float sensitivity = 0.2f;
+        rotation.y += (GetMouseY() - GetScreenHeight() / 2) * sensitivity;
+        rotation.x += (GetMouseX() - GetScreenWidth() / 2) * sensitivity;
+
+        // Limit the pitch to avoid flipping
+        if (rotation.x > 89.0f)
+            rotation.x = 89.0f;
+        if (rotation.x < -89.0f)
+            rotation.x = -89.0f;
+
+        // Set the mouse position to the center of the screen
+        // This is done to avoid the mouse leaving the window and to keep the movement smooth
+        SetMousePosition(GetScreenWidth() / 2, GetScreenHeight() / 2);
+    }
 
     // Update the camera
+    UpdateCameraPro(&(this->camera_), movement, rotation, 0.0f);
+}
 
-    this->draw_gui();
+void Visualizer::update()
+{
 
-    this->set_camera_focus();
-    UpdateCamera(&this->camera_, CAMERA_ORBITAL);
-    
+    // Update the camera
+    this->update_camera();
+
     // Draw
-    
-    BeginTextureMode(this->shader_target_);  
+    BeginTextureMode(this->shader_target_);
     // Clear the background
-    ClearBackground({ 30, 30, 30, 255 } );
+    ClearBackground({30, 30, 30, 255});
 
     BeginMode3D(this->camera_);
-    
-    for (auto& vis_object : this->visual_objects_) {
+
+    for (auto &vis_object : this->visual_objects_)
+    {
         // Get the rotation axis and angle from the quaternion
         Vector3 axis;
         float angle;
         QuaternionToAxisAngle(vis_object.orientation, &axis, &angle);
-        if (wireframe_mode_) {
+        if (wireframe_mode_)
+        {
             DrawModelWiresEx(vis_object.model, vis_object.position, axis, angle, {1.0f, 1.0f, 1.0f}, vis_object.color);
-        } else {
+        }
+        else
+        {
             DrawModelEx(vis_object.model, vis_object.position, axis, angle, {1.0f, 1.0f, 1.0f}, vis_object.color);
             // Draw wireframe with a small offset in the color (make it darker)
-            Color wireColor = { (unsigned char)(vis_object.color.r * 0.8f), 
-                                (unsigned char)(vis_object.color.g * 0.8f), 
-                                (unsigned char)(vis_object.color.b * 0.8f), 255 };
+            Color wireColor = {(unsigned char)(vis_object.color.r * 0.9f),
+                               (unsigned char)(vis_object.color.g * 0.9f),
+                               (unsigned char)(vis_object.color.b * 0.9f), 255};
             DrawModelWiresEx(vis_object.model, vis_object.position, axis, angle, {1.0f, 1.0f, 1.0f}, wireColor);
         }
-        
     }
     // Draw the axis
-    DrawGrid(100, 1.0f);
-    EndMode3D(); 
+    // DrawGrid(100, 1.0f);
+    EndMode3D();
     EndTextureMode();
-    
-    
-    BeginDrawing();
-    ClearBackground({ 30, 30, 30, 255 } );
-    
-    this->draw_shader();
-     
 
+    BeginDrawing();
+    ClearBackground({30, 30, 30, 255});
+
+    this->draw_shader();
+    this->draw_gui();
     // Draw the GUI
-    rlImGuiEnd();  
+
     EndDrawing();
-    
 }
 
 // Functions to draw geometric primitives
-uint Visualizer::add_cube(Vector3 position, Quaternion orientation, Color color, float width, float height, float length) {
+uint Visualizer::add_cube(Vector3 position, Quaternion orientation, Color color, float width, float height, float length)
+{
     Mesh cube_mesh = GenMeshCube(width, height, length);
     Model cube = LoadModelFromMesh(cube_mesh);
-    VisualObject cube_vis_object = { 
-        position, 
-        orientation, cube, color };
-    
+    VisualObject cube_vis_object = {
+        position,
+        orientation, cube, color};
+
     return this->add_visual_object(cube_vis_object);
 }
 
-uint Visualizer::add_sphere(Vector3 position, Quaternion orientation, Color color, float radius) {
+uint Visualizer::add_sphere(Vector3 position, Quaternion orientation, Color color, float radius)
+{
     Mesh sphere_mesh = GenMeshSphere(radius, 16, 16);
     Model sphere = LoadModelFromMesh(sphere_mesh);
-    VisualObject sphere_vis_object = { 
-        position, 
-        orientation, sphere, color };
-    
+    VisualObject sphere_vis_object = {
+        position,
+        orientation, sphere, color};
+
     return this->add_visual_object(sphere_vis_object);
 }
 
-uint Visualizer::add_cylinder(Vector3 position, Quaternion orientation, Color color, float radius, float height) {
+uint Visualizer::add_cylinder(Vector3 position, Quaternion orientation, Color color, float radius, float height)
+{
     Mesh cylinder_mesh = GenMeshCylinder(radius, height, 16);
     Model cylinder = LoadModelFromMesh(cylinder_mesh);
-    VisualObject cylinder_vis_object = { 
-        position, 
-        orientation, cylinder, color };
-    
+    VisualObject cylinder_vis_object = {
+        position,
+        orientation, cylinder, color};
+
     return this->add_visual_object(cylinder_vis_object);
 }
 
-uint Visualizer::add_cone(Vector3 position, Quaternion orientation, Color color, float radius, float height) {
+uint Visualizer::add_cone(Vector3 position, Quaternion orientation, Color color, float radius, float height)
+{
     Mesh cone_mesh = GenMeshCone(radius, height, 16);
     Model cone = LoadModelFromMesh(cone_mesh);
-    VisualObject cone_vis_object = { 
-        position, 
-        orientation, cone, color };
-    
+    VisualObject cone_vis_object = {
+        position,
+        orientation, cone, color};
+
     return this->add_visual_object(cone_vis_object);
 }
 
-uint Visualizer::add_plane(Vector3 position, Quaternion orientation, Color color, float width, float length) {
+uint Visualizer::add_plane(Vector3 position, Quaternion orientation, Color color, float width, float length)
+{
     Mesh plane_mesh = GenMeshPlane(width, length, 16, 16);
     Model plane = LoadModelFromMesh(plane_mesh);
-    VisualObject plane_vis_object = { 
-        position, 
-        orientation, plane, color };
-    
+    VisualObject plane_vis_object = {
+        position,
+        orientation, plane, color};
+
     return this->add_visual_object(plane_vis_object);
 }
 
-uint Visualizer::add_mesh(const char *filename, Vector3 position, Quaternion orientation, Color color, float scale) {
+uint Visualizer::add_mesh(const char *filename, Vector3 position, Quaternion orientation, Color color, float scale)
+{
     Model model = LoadModel(filename);
     model.transform = MatrixScale(scale, scale, scale);
-    VisualObject vis_object = { 
-        position, 
-        orientation, model, color };
-    
+    VisualObject vis_object = {
+        position,
+        orientation, model, color};
+
     return this->add_visual_object(vis_object);
 }
 
 // uint Visualizer::add_heightmap(Vector3 position, Quaternion orientation, Color color, std::vector<std::vector<float>> heightmap) {
 //     Mesh heightmap_mesh = GenMeshHeightmap(heightmap, 1.0f, 1.0f, 1.0f);
 //     Model heightmap_model = LoadModelFromMesh(heightmap_mesh);
-//     VisualObject heightmap_vis_object = { 
-//         position, 
+//     VisualObject heightmap_vis_object = {
+//         position,
 //         orientation, heightmap_model, color };
-    
+
 //     return this->add_visual_object(heightmap_vis_object);
 // }
-void Visualizer::set_up_lighting() {
-    this->light_ = CreateLight(LIGHT_DIRECTIONAL, (Vector3){ 0.0f, 3.0f, 0.0f }, Vector3Zero(), WHITE, this->shader_);
+void Visualizer::set_up_lighting()
+{
+    this->light_ = CreateLight(LIGHT_DIRECTIONAL, (Vector3){0.0f, 3.0f, 0.0f}, Vector3Zero(), WHITE, this->shader_);
 
     this->shader_.locs[SHADER_LOC_VECTOR_VIEW] = GetShaderLocation(this->shader_, "viewPos");
-    
+
     // Ambient light level (some basic lighting)
     int ambientLoc = GetShaderLocation(this->shader_, "ambient");
-    Vector4 ambient = { 1.0f, 1.0f, 1.0f, 1.0f };  // Use Vector4 for ambient light
+    Vector4 ambient = {1.0f, 1.0f, 1.0f, 1.0f}; // Use Vector4 for ambient light
     SetShaderValue(this->shader_, ambientLoc, &ambient, SHADER_UNIFORM_VEC4);
 
     // Assign out lighting shader to model
 
-    for (auto& vis_object : this->visual_objects_) {
+    for (auto &vis_object : this->visual_objects_)
+    {
         vis_object.model.materials[0].shader = this->shader_;
     }
-
 }
 
-
-void Visualizer::load_shader(const char* filename_vs, const char* filename_fs){
+void Visualizer::load_shader(const char *filename_vs, const char *filename_fs, int group_id)
+{
     this->shader_ = LoadShader(TextFormat(filename_vs, GLSL_VERSION), TextFormat(filename_fs, GLSL_VERSION));
     this->shader_loaded_ = true;
 }
 
-void Visualizer::close() {
+void Visualizer::close()
+{
     // Unload all the models
-    for (auto& vis_object : this->visual_objects_) {
+    for (auto &vis_object : this->visual_objects_)
+    {
         UnloadModel(vis_object.model);
     }
     rlImGuiShutdown();
--- src/Visualizer.hpp
+++ src/Visualizer.hpp
@@ -1,68 +1,222 @@
 
 #pragma once
-#include<raylib.h>
-#include<raymath.h>
-#include<vector>
-#include"rlImGui.h"	
-#include"imgui.h"
-#include"rlights.hpp"
-#include<iostream>
+#include <raylib.h>
+#include <raymath.h>
+#include <vector>
+#include "rlImGui.h"
+#include "imgui.h"
+#include "rlights.hpp"
+#include <iostream>
 
-#define GLSL_VERSION            330
+#define GLSL_VERSION 330
 
-struct VisualObject {
-    Vector3 position;
-    Quaternion orientation;
-    Model model;
-    Color color; 
-    uint group_id = 0;
+/**
+ * @brief Represents a 3D visual object with position, orientation, model, color, and group ID.
+ */
+struct VisualObject
+{
+    Vector3 position;       // Position of the visual object.
+    Quaternion orientation; // Orientation of the visual object.
+    Model model;            // Model associated with the visual object.
+    Color color;            // Color of the visual object.
+    uint group_id = 0;      // Group ID to which the visual object belongs.
 };
 
-class Visualizer {
-    private:
-        const int screen_width_;
-        const int screen_height_;
-        const char* title_;
-        Camera camera_;
-        Shader shader_;
-        bool shader_loaded_ = false;
-        std::vector<VisualObject> visual_objects_;
-        bool wireframe_mode_;
-        int focused_object_index_;
-        RenderTexture2D shader_target_;
-        Light light_;
+/**
+ * @brief Manages the visualization of 3D objects, camera, lighting, and shaders.
+ */
+class Visualizer
+{
+private:
+    const int screen_width_;                   // Screen width for visualization.
+    const int screen_height_;                  // Screen height for visualization.
+    const char *title_;                        // Title of the visualization window.
+    Camera camera_;                            // Camera for viewing the scene.
+    Shader shader_;                            // Shader used for rendering.
+    bool shader_loaded_ = false;               // Flag indicating whether the shader is loaded.
+    std::vector<VisualObject> visual_objects_; // List of visual objects in the scene.
+    bool wireframe_mode_;                      // Flag indicating whether to render in wireframe mode.
+    int focused_object_index_;                 // Index of the focused visual object.
+    RenderTexture2D shader_target_;            // Render target for shaders.
+    Light light_;                              // Lighting setup for the scene.
 
-    public:
-        Visualizer(int screen_width, int screen_height, const char* title);
-        ~Visualizer();
-        void update();
-        void draw_gui();
-        void draw_shader();
-        void close();
-        // Camera
-        void set_up_camera();
-        void set_camera_focus();
-        // Lighting
-        void set_up_lighting();
-        // Shader
-        // filename -> path to the shader file (.fs)
-        void load_shader(const char* filename_fs, const char* filename_vs);
-        //--------------------//
-        uint add_visual_object(VisualObject vis_object);
-        void update_visual_object_position_orientation_scale(uint index, Vector3 position, Quaternion orientation, Vector3 scale);
-        void update_visual_object_position_orientation(uint index, Vector3 position, Quaternion orientation);
-        void update_visual_object_scale(uint index, Vector3 scale);
-        void remove_visual_object(uint index);
-        void clear_visual_objects();
-        // Functions to draw geometric primitives
-        uint add_cube(Vector3 position, Quaternion orientation, Color color, float width, float height, float length);
-        uint add_sphere(Vector3 position, Quaternion orientation, Color color, float radius);
-        uint add_cylinder(Vector3 position, Quaternion orientation, Color color, float radius, float height);
-        uint add_cone(Vector3 position, Quaternion orientation, Color color, float radius, float height);
-        uint add_plane(Vector3 position, Quaternion orientation, Color color, float width, float length);
-        //uint add_heightmap(Vector3 position, Quaternion orientation, Color color, std::vector<std::vector<float>> heightmap);
-        uint add_mesh(const char *filename, Vector3 position, Quaternion orientation, Color color, float scale);
-        
+public:
+    /**
+     * @brief Constructor for the Visualizer class.
+     * @param screen_width The width of the visualization window.
+     * @param screen_height The height of the visualization window.
+     * @param title Title of the visualization window.
+     */
+    Visualizer(int screen_width, int screen_height, const char *title);
 
+    /**
+     * @brief Destructor for the Visualizer class.
+     */
+    ~Visualizer();
 
+    /**
+     * @brief Updates the visualizer, including camera and shader updates.
+     */
+    void update();
+
+    /**
+     * @brief Draws the graphical user interface (GUI) for the visualizer.
+     */
+    void draw_gui();
+
+    /**
+     * @brief Draws the shader effects on the scene.
+     */
+    void draw_shader();
+
+    /**
+     * @brief Closes the visualization window.
+     */
+    void close();
+
+    // Camera
+    /**
+     * @brief Sets up the camera with default parameters.
+     */
+    void set_up_camera();
+
+    /**
+     * @brief Updates the camera based on the keyboard and mouse inputs.
+     */
+    void update_camera();
+
+    /**
+     * @brief Sets the camera focus based on the focused object.
+     */
+    void set_camera_focus();
+
+    // Lighting
+    /**
+     * @brief Sets up the lighting for the scene.
+     */
+    void set_up_lighting();
+
+    // Shader
+    /**
+     * @brief Loads and sets the shader for rendering.
+     * @param filename_fs Path to the fragment shader file.
+     * @param filename_vs Path to the vertex shader file.
+     * @param group_id Group ID associated with the shader.
+     */
+    void load_shader(const char *filename_fs, const char *filename_vs, int group_id);
+
+    //--------------------//
+
+    /**
+     * @brief Adds a visual object to the scene.
+     * @param vis_object The visual object to be added.
+     * @return The index of the added visual object.
+     */
+    uint add_visual_object(VisualObject vis_object);
+
+    /**
+     * @brief Updates the position, orientation, and scale of a visual object.
+     * @param index Index of the visual object to be updated.
+     * @param position New position of the visual object.
+     * @param orientation New orientation of the visual object.
+     * @param scale New scale of the visual object.
+     */
+    void update_visual_object_position_orientation_scale(uint index, Vector3 position, Quaternion orientation, Vector3 scale);
+
+    /**
+     * @brief Updates the position and orientation of a visual object.
+     * @param index Index of the visual object to be updated.
+     * @param position New position of the visual object.
+     * @param orientation New orientation of the visual object.
+     */
+    void update_visual_object_position_orientation(uint index, Vector3 position, Quaternion orientation);
+
+    /**
+     * @brief Updates the scale of a visual object.
+     * @param index Index of the visual object to be updated.
+     * @param scale New scale of the visual object.
+     */
+    void update_visual_object_scale(uint index, Vector3 scale);
+
+    /**
+     * @brief Removes a visual object from the scene.
+     * @param index Index of the visual object to be removed.
+     */
+    void remove_visual_object(uint index);
+
+    /**
+     * @brief Clears all visual objects from the scene.
+     */
+    void clear_visual_objects();
+
+    // Functions to draw geometric primitives
+
+    /**
+     * @brief Adds a cube to the scene.
+     * @param position Position of the cube.
+     * @param orientation Orientation of the cube.
+     * @param color Color of the cube.
+     * @param width Width of the cube.
+     * @param height Height of the cube.
+     * @param length Length of the cube.
+     * @return The index of the added cube.
+     */
+    uint add_cube(Vector3 position, Quaternion orientation, Color color, float width, float height, float length);
+
+    /**
+     * @brief Adds a sphere to the scene.
+     * @param position Position of the sphere.
+     * @param orientation Orientation of the sphere.
+     * @param color Color of the sphere.
+     * @param radius Radius of the sphere.
+     * @return The index of the added sphere.
+     */
+    uint add_sphere(Vector3 position, Quaternion orientation, Color color, float radius);
+
+    /**
+     * @brief Adds a cylinder to the scene.
+     * @param position Position of the cylinder.
+     * @param orientation Orientation of the cylinder.
+     * @param color Color of the cylinder.
+     * @param radius Radius of the cylinder.
+     * @param height Height of the cylinder.
+     * @return The index of the added cylinder.
+     */
+    uint add_cylinder(Vector3 position, Quaternion orientation, Color color, float radius, float height);
+
+    /**
+     * @brief Adds a cone to the scene.
+     * @param position Position of the cone.
+     * @param orientation Orientation of the cone.
+     * @param color Color of the cone.
+     * @param radius Radius of the cone.
+     * @param height Height of the cone.
+     * @return The index of the added cone.
+     */
+    uint add_cone(Vector3 position, Quaternion orientation, Color color, float radius, float height);
+
+    /**
+     * @brief Adds a plane to the scene.
+     * @param position Position of the plane.
+     * @param orientation Orientation of the plane.
+     * @param color Color of the plane.
+     * @param width Width of the plane.
+     * @param length Length of the plane.
+     * @return The index of the added plane.
+     */
+    uint add_plane(Vector3 position, Quaternion orientation, Color color, float width, float length);
+
+    // Uncomment the following line when the heightmap function is implemented.
+    // uint add_heightmap(Vector3 position, Quaternion orientation, Color color, std::vector<std::vector<float>> heightmap);
+
+    /**
+     * @brief Adds a mesh to the scene.
+     * @param filename Path to the mesh file.
+     * @param position Position of the mesh.
+     * @param orientation Orientation of the mesh.
+     * @param color Color of the mesh.
+     * @param scale Scale of the mesh.
+     * @return The index of the added mesh.
+     */
+    uint add_mesh(const char *filename, Vector3 position, Quaternion orientation, Color color, float scale);
 };
